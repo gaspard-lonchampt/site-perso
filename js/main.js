1d179acff5c2389560165fe62fe8a575e131fd2b
Gaspard "Sarp" Lonchampt <71872843+gaspard-lonchampt@users.noreply.github.com>
2021-03-22 12:47:57 +0100

    tabs en cours

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// navigation mobile
 
 $(document).ready(function () {
   // Check for click events on the navbar burger icon
@@ -8,8 +9,23 @@ $(document).ready(function () {
   });
 });
 
+// animation en fonction du scroll
+
+window.addEventListener(
+  "scroll",
+  () => {
+    document.body.style.setProperty(
+      "--scroll",
+      window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
+    );
+  },
+  false
+);
+
+// animation page d'accueil 
+
 anime
-  .timeline({ loop : true })
+  .timeline({ loop: true })
   .add({
     targets: ".ml8 .circle-white",
     scale: [0, 3],
@@ -59,4 +75,50 @@ anime({
   duration: 8000,
   easing: "linear",
   loop: true,
+});
+
+// animation tabs 
+
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function () {
+  var tabs = document.getElementsByClassName("tabs");
+  if (tabs) {
+    var _loop = function _loop() {
+      var tabListItems = tabs[i].querySelectorAll("li");
+      tabListItems.forEach(function (tabListItem) {
+        // création d'un écouteur d'évènements sur le clic d'une tab
+        tabListItem.addEventListener(
+          "click",
+          function () {
+            // suppression de la classe is-active sur chacune des tabs avant de la rajouter sur la tab qui a été cliquée
+            tabListItems.forEach(function (tabListItem) {
+              tabListItem.classList.remove("is-active");
+            });
+            tabListItem.classList.add("is-active");
+
+            // tabName correspond à la valeur de l'attribut data-tab
+            var tabName = tabListItem.dataset.tab;
+
+            // on identifie tous les contenus possibles puis on applique la classe has-display-none si l'ID du contenu ne correspond pas à la valeur de l'attribut data-tab
+            tabListItem
+              .closest(".js-tabs-container")
+              .querySelectorAll(".js-tab-content")
+              .forEach(function (tabContent) {
+                if (tabContent.id !== tabName) {
+                  tabContent.classList.add("has-display-none");
+                } else {
+                  tabContent.classList.remove("has-display-none");
+                }
+              });
+          },
+          false
+        );
+      });
+    };
+
+    for (var i = 0; i < tabs.length; i++) {
+      _loop();
+    }
+  }
 });
