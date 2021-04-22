ba9a34f70bb3460f82095510c0f0008d3e79044d
Gaspard "Sarp" Lonchampt <71872843+gaspard-lonchampt@users.noreply.github.com>
2021-04-22 15:26:42 +0200

    V 1.0

--- js/main.js
+++ js/main.js
@@ -7,90 +7,97 @@ $(document).ready(function () {
     $(".navbar-burger").toggleClass("is-active");
     $(".navbar-menu").toggleClass("is-active");
   });
-});
 
-// animation en fonction du scroll
-
-window.addEventListener(
-  "scroll",
-  () => {
-    document.body.style.setProperty(
-      "--scroll",
-      window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
-    );
-  },
-  false
-);
-
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function () {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("nav_bar_js").style.transform = "translate(0, 0px)";
+  if ($(window).width() < 768) {
+    $("#tabs_nav").removeClass("is-large").addClass("is-small");
   } else {
-    document.getElementById("nav_bar_js").style.transform = "translate(0, -50px)" ;
+    $("#tabs_nav").removeClass("is-small").addClass("is-large");
   }
-  prevScrollpos = currentScrollPos;
-}; 
+});
+  // animation en fonction du scroll
 
-// animation page d'accueil 
+  window.addEventListener(
+    "scroll",
+    () => {
+      document.body.style.setProperty(
+        "--scroll",
+        window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
+      );
+    },
+    false
+  );
 
-anime
-  .timeline({ loop: true })
-  .add({
-    targets: ".ml8 .circle-white",
-    scale: [0, 3],
-    opacity: [1, 0],
-    easing: "easeInOutExpo",
+  var prevScrollpos = window.pageYOffset;
+  window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      document.getElementById("nav_bar_js").style.transform =
+        "translate(0, 0px)";
+    } else {
+      document.getElementById("nav_bar_js").style.transform =
+        "translate(0, -300px)";
+    }
+    prevScrollpos = currentScrollPos;
+  };
+
+  // animation page d'accueil
+
+  anime
+    .timeline({ loop: true })
+    .add({
+      targets: ".ml8 .circle-white",
+      scale: [0, 3],
+      opacity: [1, 0],
+      easing: "easeInOutExpo",
+      rotateZ: 360,
+      duration: 1100,
+    })
+    .add({
+      targets: ".ml8 .circle-container",
+      scale: [0, 1],
+      duration: 1100,
+      easing: "easeInOutExpo",
+      offset: "-=1000",
+    })
+    .add({
+      targets: ".ml8 .circle-dark",
+      scale: [0, 1],
+      duration: 1100,
+      easing: "easeOutExpo",
+      offset: "-=600",
+    })
+    .add({
+      targets: ".ml8 .letters-left",
+      scale: [0, 1],
+      duration: 1200,
+      offset: "-=550",
+    })
+    .add({
+      targets: ".ml8 .bang",
+      scale: [0, 1],
+      rotateZ: [45, 15],
+      duration: 1800,
+      offset: "-=1000",
+    })
+    .add({
+      targets: ".ml8",
+      opacity: 0,
+      duration: 1000,
+      easing: "easeOutExpo",
+      delay: 1400,
+    });
+
+  anime({
+    targets: ".ml8 .circle-dark-dashed",
     rotateZ: 360,
-    duration: 1100,
-  })
-  .add({
-    targets: ".ml8 .circle-container",
-    scale: [0, 1],
-    duration: 1100,
-    easing: "easeInOutExpo",
-    offset: "-=1000",
-  })
-  .add({
-    targets: ".ml8 .circle-dark",
-    scale: [0, 1],
-    duration: 1100,
-    easing: "easeOutExpo",
-    offset: "-=600",
-  })
-  .add({
-    targets: ".ml8 .letters-left",
-    scale: [0, 1],
-    duration: 1200,
-    offset: "-=550",
-  })
-  .add({
-    targets: ".ml8 .bang",
-    scale: [0, 1],
-    rotateZ: [45, 15],
-    duration: 1800,
-    offset: "-=1000",
-  })
-  .add({
-    targets: ".ml8",
-    opacity: 0,
-    duration: 1000,
-    easing: "easeOutExpo",
-    delay: 1400,
+    duration: 8000,
+    easing: "linear",
+    loop: true,
   });
 
-anime({
-  targets: ".ml8 .circle-dark-dashed",
-  rotateZ: 360,
-  duration: 8000,
-  easing: "linear",
-  loop: true,
-});
+// animation tabs
 
-// animation tabs 
-
-"use strict";
+("use strict");
 
 document.addEventListener("DOMContentLoaded", function () {
   var tabs = document.getElementsByClassName("tabs");
@@ -133,3 +140,8 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 });
+
+// animation scroll reveal
+
+ScrollReveal().reveal(".text_headline", { duration: 2500 });
+ScrollReveal().reveal(".text_headline", { interval: 500 });
